66560b3fa8ca1c94ecada9949b601a8c96174f05
Kevin <kclarkcuadrado@gmail.com>
2022-03-05 16:52:09 -0500

    Renamings

---
+++ test_queries.py
@@ -0,0 +1,5 @@
+from time import perf_counter
+from typing import List
+from RangeQueries import AlternateMinMax, BruteForceMinMax
+from testing_utils import get_random_arr, get_random_query, get_separated_random_arr
+from hypothesis import given
